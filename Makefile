e3e6798eb7d60eb09c8cea642ad45544dd2d0f9c
Jake Rosoman <jkroso@gmail.com>
2015-12-25 17:41:35 +1300

    use es6

--- Makefile
+++ Makefile
@@ -1,13 +1,7 @@
-
 serve: node_modules
-	@node_modules/serve/bin/serve -Slojp 0
+	@$</.bin/serve -Slojp 0
 
-test: node_modules
-	@sed "s/'timeline'/'.\/'/" Readme.md | node_modules/jsmd/bin/jsmd
+node_modules: package.json
+	@npm install
 
-node_modules: *.json
-	@packin install \
-		--meta deps.json,package.json \
-		--folder node_modules
-
-.PHONY: serve test
+.PHONY: serve
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-
-var Animation = require('animation')
+import Animation from '@jkroso/animation'
 
 /**
  * create a timeline
@@ -8,84 +7,78 @@ var Animation = require('animation')
  * @return {Timeline}
  */
 
-module.exports = function(anims){
-  var line = new Timeline
+const timeline = anims => {
+  const line = new Timeline([])
   if (anims) for (var i = 0; i < anims.length;) {
     line.add.apply(line, anims[i++])
   }
   return line
 }
 
-function Timeline(){
-  this.animations = []
-}
+export default timeline
 
-/**
- * inherit from Animation
- */
-
-Animation.extend(Timeline)
-
-/**
- * add an animation to the timeline
- *
- * @param {Number} start
- * @param {Number} end
- * @param {Animation} anim
- * @return {this}
- */
-
-Timeline.prototype.add = function(start, end, anim){
-  if (arguments.length == 1) {
-    anim = start
-    start = 0
-    end = 1
+export class Timeline extends Animation {
+  constructor(animations) {
+    super()
+    this.animations = animations
   }
-  this.animations.push([anim, start, end])
-  return this
-}
 
-/**
- * render all animations at point `n` on the timeline
- *
- * @param {Number} n
- * @return {this}
- */
+  /**
+  * add an animation to the timeline
+  *
+  * @param {Number} start
+  * @param {Number} end
+  * @param {Animation} anim
+  * @return {this}
+  */
 
-Timeline.prototype.render = function(n){
-  n = this._ease(n)
-  var anims = this.animations
-  var i = anims.length
-  while (i--) {
-    var anim = anims[i]
-    var from = anim[1]
-    var to = anim[2]
-    if (from > n) anim[0].render(0)
-    else if (to < n) anim[0].render(1)
-    else anim[0].render((n - from) / (to - from))
-  }
-  return this
-}
-
-/**
- * create a new timeline which will start when this
- * one complete
- *
- * @param {Animation|Function} [anim]
- * @return {Timeline}
- */
-
-Timeline.prototype.then = function(anim){
-  if (anim != null) {
-    var fn = typeof anim != 'function'
-      ? function(){ anim.run() }
-      : anim
-    this.on('end', fn)
-    this.running || this.parent || this.run()
+  add(start, end, anim) {
+    if (arguments.length == 1) {
+      anim = start
+      start = 0
+      end = 1
+    }
+    this.animations.push([anim, start, end])
     return this
   }
-  anim = new Timeline
-  anim.parent = this
-  this.then(anim)
-  return anim
+
+  /**
+  * render all animations at point `n` on the timeline
+  *
+  * @param {Number} n
+  * @return {this}
+  */
+
+  render(n) {
+    n = this._ease(n)
+    const anims = this.animations
+    var i = anims.length
+    while (i--) {
+      const [anim, from, to] = anims[i]
+      if (from > n) anim.render(0)
+      else if (to < n) anim.render(1)
+      else anim.render((n - from) / (to - from))
+    }
+    return this
+  }
+
+  /**
+  * create a new timeline which will start when this
+  * one complete
+  *
+  * @param {Animation|Function} [anim]
+  * @return {Timeline}
+  */
+
+  then(anim) {
+    if (anim != null) {
+      this.on('end', typeof anim != 'function' ? () => anim.run() : anim)
+      this.running || this.parent || this.run()
+      return this
+    }
+    anim = new Timeline([])
+    anim.parent = this
+    this.then(anim)
+    return anim
+  }
 }
